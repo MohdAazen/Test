5402aa8c80310fbd3eb798b262df5767fc37cfdd
Aazen <sayyedaazen81@gmail.com>
2023-09-19 11:25:24 +0530

    commit

---
+++ Youtube JS/day3.js
@@ -0,0 +1,87 @@
+// Switch
+// let stat = 1;
+// switch(stat)
+// {
+//     case 1:
+//         {
+//             console.log("Super User");
+//             break;
+//         }
+//     case 2:
+//         {
+//             console.log("Medium User");
+//             break;
+//         }
+//     case 3:
+//         {
+//             console.log("Normal User");
+//             break;
+//         }
+//         default:
+//             console.log("Lower Level User");
+// }
+
+// Ternary Operator
+// let vote = null;
+
+// let age = 26;
+// if(age > 18)
+// {
+//     vote = true;
+// }
+// else
+// {
+//     vote = false;
+// }
+// console.log(vote);
+
+// let age =16;
+// let votestatus = age>18? true: false;
+// console.log(votestatus);
+
+// Loop
+
+// let a = 1;
+
+// while(a<=10)
+// {
+//     console.log(`the num is ${a}`);
+//     ++a;
+// }
+
+// do{
+//     console.log(`the num is ${a}`);
+//     a++;
+// }
+// while(a<=10);
+
+// for(let i=1;i<=10;i++)
+// {
+//     console.log(`the num is ${i}`);
+// }
+
+// Collection of elements of same type stored in single variable
+// Array
+
+// let cities=["Mumbai","Pune","Hyderabad"];
+// for(let i = 0;i<cities.length;i++)
+// {
+// console.log(cities[i]);
+// }
+// console.log(cities);
+
+// Object
+// Collection of properties/data which is representing a single entity 
+let student1 = {
+    name: "Aazen",
+    age:27,
+    city: "Mumbai"
+}
+let student2 = {
+    name: "Kalim",
+    age:47,
+    city: "Thane"
+}
+
+console.log(student1);
+
